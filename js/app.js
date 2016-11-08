21d46ce7343c31aeb335637832c1ca1569fbc73c
CarolMadeleine <karitomadeleine@gmail.com>
2016-11-07 20:46:33 -0500

    subiendo lyft -comienzo

---
+++ js/app.js
@@ -0,0 +1,33 @@
+window.addEventListener("load", function(){
+	var portada = document.getElementById("portada");
+	var btnLogIn = document.getElementById("btnLogIn");
+	var btnSignUp = document.getElementById("btnSignUp");
+	var signUp = document.getElementById("signUp");
+	var signUpDatos = document.getElementById("signUpDatos");
+	var iconBack = document.getElementById("iconBack");
+	var btnNext = document.getElementById("btnNext");
+	var iconBackDatos = document.getElementById("iconBackDatos");
+
+	btnSignUp.addEventListener("click", portadaHidden);
+	iconBack.addEventListener("click", portadaAppears);
+	btnNext.addEventListener("click", SingUpDatos);
+	iconBackDatos.addEventListener("click", SingUpHidden);
+
+});
+
+function portadaHidden(e){
+	portada.classList.add("hidden");
+	signUp.classList.remove("hidden");
+}
+function portadaAppears(e) {
+	portada.classList.remove("hidden");
+	signUp.classList.add("hidden");
+}
+function SingUpDatos(e){
+	signUp.classList.add("hidden");
+	signUpDatos.classList.remove("hidden");
+}
+function SingUpHidden(e){
+	signUp.classList.remove("hidden");
+	signUpDatos.classList.add("hidden");
+}
